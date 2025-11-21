a70ded18c2c1a88e533efafc4e8f094041f52832
ktan <ktan247610@gmail.com>
2025-11-21 15:30:42 +0530

    binance US url addd

--- src/connectors/binance.rs
+++ src/connectors/binance.rs
@@ -16,7 +16,7 @@ struct BinanceTrade {
 
 pub async fn run_binance_connector(tx: Sender<PriceUpdate>, pair: String) {
     let symbol = pair.to_lowercase().replace("/", "");
-    let url = format!("wss://stream.binance.com:9443/ws/{}@trade", symbol);
+    let url = format!("wss://stream.binance.us:9443/ws/{}@trade", symbol);
 
     println!("Connecting to Binance: {}", url);
 
@@ -68,4 +68,3 @@ pub async fn run_binance_connector(tx: Sender<PriceUpdate>, pair: String) {
         Err(e) => eprintln!("Binance connection error: {:?}", e),
     }
 }
-
